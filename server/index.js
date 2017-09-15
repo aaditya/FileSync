6dc500b4fcfd0ff2e03df5ef8fdd26c7690f50de
Aaditya Chakravarty <ac36500@gmail.com>
2017-09-15 07:51:38 +0530

    Alpha

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var request = require('request');
+var fs = require('fs');
+
 var server = require('http').createServer(app);
 var port = process.env.PORT || 5000;
-var fs = require('fs');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -18,16 +20,46 @@ app.use(function(req, res, next) {
 
 app.use(express.static('public'));
 
+var parentDir = __dirname+'/public';
+
+fs.watch(parentDir, {recursive : true}, function(eventType, filename){
+	//console.log('event type is: '+eventType);
+	if(filename){
+		//console.log('filename provided: '+filename);
+		if(eventType == 'change'){
+      /*Notice changes, generate a json request and send it to cloud.*/
+      fs.readFile(parentDir+'/'+filename, 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        var changes = {
+          "edit":true,
+          "file":filename,
+          "content": data
+        };
+        request.post('http://localhost:3000/sync',{json: changes},
+          function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              console.log(body)
+            }
+            console.log('Change Synchronised: '+filename)
+          });
+      });
+	  }
+}
+});
+
 app.post('/sync',function(req,res){
 	var file = {
 		"name":req.body.file,
-		"content":req.body.content
+		"content":req.body.content,
+    "sync":req.body.edit
 	};
 	fs.writeFile(__dirname+'/public/'+file.name, file.content, function(err) {
   	if(err) {
         return console.log(err);
     }
-		console.log("The file was synchronised.");
+		console.log("Change Synchronised: "+file.name);
 	});
 });
 
